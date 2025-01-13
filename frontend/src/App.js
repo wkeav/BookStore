09de58935cdb777cf3e314989b166698afb5cc2b
Astra <wkeavos0513@mail.com>
2025-01-12 18:01:13 -0600

    Updated index & app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
     return (
-    <div> 
-      <h1> Hello world!</h1>
-    </div>
+      <div className="App">
+        <Navbar/>
+      </div>
     );
 }
-
 export default App;
