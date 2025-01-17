0bca7b43ac2c0d2656b6fb6d758b31bfe1df4b6c
Astra <astra.k.nguyen05@outlook.com>
2025-01-17 12:17:18 -0600

    Created a BookService to get all book data from backend to frontend

---
+++ frontend/src/services/BookService.js
@@ -0,0 +1,26 @@
+import Books from "../components/Books";
+
+/*
+* Provides functions to interact with backend API for book data 
+*/
+const API_URL = 'http://localhost:8080/api/books';
+
+const BookService =  {
+    //fetch all books in JSON form
+    getAllBooks: async () => {
+        const response = await fetch(API_URl);
+        if(!response.ok){
+            throw new Error('Failed to fetch all books');
+        }
+        return response.json();
+    },
+    //fetch a book id
+    getBookById: async(id) => {
+        const response = await fetch (`${API_URL}/${id}`);
+        if(!response.ok){
+            throw new Error('Failed to fetch book');
+        }
+        return response.json();
+    }
+};
+export default BookService; 
