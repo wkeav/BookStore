44641b773e8d7c7a53dad2003ac5e7eb5bd960f2
Astra <astra.k.nguyen05@outlook.com>
2025-01-17 12:16:36 -0600

    provided a user authentication context, to check if a user is login/logout

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,37 @@
+import React, {createContext, useState, useContext, useSyncExternalStore} from 'react'; 
+/*
+* Tell authentication state across application; telling the application when a user is logged in/logged out
+*/
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ props }) => {
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false); // dynamic user authentication 
+    const [user, setUser] = useState(null); //user information 
+
+    //when a user logs in
+    const login = (userData) => {
+        setIsAuthenticated(true); 
+        setUser(userData); //save their info
+    };
+
+    //when user logs out 
+    const logout = () => {
+        setIsAuthenticated(false);
+        setUser(null); //remove their info
+    };
+
+    return(
+        //context provider component that will be wrapped around the application
+        <AuthContext.Provider 
+            value={{isAuthenticated, user, login, logout}}
+        >
+            {props}
+        </AuthContext.Provider>
+    );
+};
+
+//useContext is a React hook that gives the current context for AuthContext
+// in this case, it is isAuthenticated, user, login & logout 
+//this allow other components in React, to access the authentication context 
+export const useAuth = () => useContext(AuthContext);
